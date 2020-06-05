2cb4fb2855aab913be595ad6f9c816382e996458
thoitbk <trananhtho207@gmail.com>
2020-06-06 00:01:50 +0700

    detect fault in card

--- 896 Tool/Form1.Designer.cs
+++ 896 Tool/Form1.Designer.cs
@@ -37,6 +37,20 @@
             this.folderButton = new System.Windows.Forms.Button();
             this.loadButton = new System.Windows.Forms.Button();
             this.addressTextbox = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.checkLabel = new System.Windows.Forms.Label();
+            this.checkTextbox = new System.Windows.Forms.TextBox();
+            this.DC02FolderButton = new System.Windows.Forms.Button();
+            this.DC02Label = new System.Windows.Forms.Label();
+            this.DC02Textbox = new System.Windows.Forms.TextBox();
+            this.DC02Button = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // fileTextBox
@@ -69,7 +83,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(24, 152);
+            this.button1.Location = new System.Drawing.Point(98, 95);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(164, 38);
             this.button1.TabIndex = 3;
@@ -80,7 +94,7 @@
             // folderTextbox
             // 
             this.folderTextbox.BackColor = System.Drawing.SystemColors.Window;
-            this.folderTextbox.Location = new System.Drawing.Point(75, 51);
+            this.folderTextbox.Location = new System.Drawing.Point(61, 20);
             this.folderTextbox.Name = "folderTextbox";
             this.folderTextbox.ReadOnly = true;
             this.folderTextbox.Size = new System.Drawing.Size(201, 20);
@@ -89,7 +103,7 @@
             // folderLabel
             // 
             this.folderLabel.AutoSize = true;
-            this.folderLabel.Location = new System.Drawing.Point(21, 54);
+            this.folderLabel.Location = new System.Drawing.Point(6, 23);
             this.folderLabel.Name = "folderLabel";
             this.folderLabel.Size = new System.Drawing.Size(49, 13);
             this.folderLabel.TabIndex = 1;
@@ -97,7 +111,7 @@
             // 
             // folderButton
             // 
-            this.folderButton.Location = new System.Drawing.Point(301, 49);
+            this.folderButton.Location = new System.Drawing.Point(277, 18);
             this.folderButton.Name = "folderButton";
             this.folderButton.Size = new System.Drawing.Size(92, 23);
             this.folderButton.TabIndex = 2;
@@ -107,9 +121,9 @@
             // 
             // loadButton
             // 
-            this.loadButton.Location = new System.Drawing.Point(24, 114);
+            this.loadButton.Location = new System.Drawing.Point(85, 67);
             this.loadButton.Name = "loadButton";
-            this.loadButton.Size = new System.Drawing.Size(97, 23);
+            this.loadButton.Size = new System.Drawing.Size(252, 23);
             this.loadButton.TabIndex = 4;
             this.loadButton.Text = "Tải danh sách";
             this.loadButton.UseVisualStyleBackColor = true;
@@ -117,27 +131,146 @@
             // 
             // addressTextbox
             // 
-            this.addressTextbox.Location = new System.Drawing.Point(75, 77);
+            this.addressTextbox.Location = new System.Drawing.Point(61, 55);
             this.addressTextbox.Name = "addressTextbox";
             this.addressTextbox.Size = new System.Drawing.Size(201, 20);
             this.addressTextbox.TabIndex = 5;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 103);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(404, 180);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.folderButton);
+            this.tabPage1.Controls.Add(this.addressTextbox);
+            this.tabPage1.Controls.Add(this.folderLabel);
+            this.tabPage1.Controls.Add(this.folderTextbox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(396, 154);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Tạo file excel kết quả kiểm tra";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.DC02Button);
+            this.tabPage2.Controls.Add(this.DC02FolderButton);
+            this.tabPage2.Controls.Add(this.DC02Label);
+            this.tabPage2.Controls.Add(this.DC02Textbox);
+            this.tabPage2.Controls.Add(this.checkButton);
+            this.tabPage2.Controls.Add(this.checkLabel);
+            this.tabPage2.Controls.Add(this.checkTextbox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(396, 154);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Sinh DC02";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Địa chỉ";
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(285, 21);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(92, 23);
+            this.checkButton.TabIndex = 5;
+            this.checkButton.Text = "Chọn file";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // checkLabel
+            // 
+            this.checkLabel.AutoSize = true;
+            this.checkLabel.Location = new System.Drawing.Point(6, 26);
+            this.checkLabel.Name = "checkLabel";
+            this.checkLabel.Size = new System.Drawing.Size(48, 13);
+            this.checkLabel.TabIndex = 4;
+            this.checkLabel.Text = "Chọn file";
+            // 
+            // checkTextbox
+            // 
+            this.checkTextbox.BackColor = System.Drawing.SystemColors.Window;
+            this.checkTextbox.Location = new System.Drawing.Point(59, 23);
+            this.checkTextbox.Name = "checkTextbox";
+            this.checkTextbox.ReadOnly = true;
+            this.checkTextbox.Size = new System.Drawing.Size(201, 20);
+            this.checkTextbox.TabIndex = 3;
+            // 
+            // DC02FolderButton
+            // 
+            this.DC02FolderButton.Location = new System.Drawing.Point(285, 56);
+            this.DC02FolderButton.Name = "DC02FolderButton";
+            this.DC02FolderButton.Size = new System.Drawing.Size(92, 23);
+            this.DC02FolderButton.TabIndex = 8;
+            this.DC02FolderButton.Text = "Chọn thư mục";
+            this.DC02FolderButton.UseVisualStyleBackColor = true;
+            this.DC02FolderButton.Click += new System.EventHandler(this.DC02FolderButton_Click);
+            // 
+            // DC02Label
+            // 
+            this.DC02Label.AutoSize = true;
+            this.DC02Label.Location = new System.Drawing.Point(4, 61);
+            this.DC02Label.Name = "DC02Label";
+            this.DC02Label.Size = new System.Drawing.Size(49, 13);
+            this.DC02Label.TabIndex = 7;
+            this.DC02Label.Text = "Thư mục";
+            // 
+            // DC02Textbox
+            // 
+            this.DC02Textbox.BackColor = System.Drawing.SystemColors.Window;
+            this.DC02Textbox.Location = new System.Drawing.Point(59, 58);
+            this.DC02Textbox.Name = "DC02Textbox";
+            this.DC02Textbox.ReadOnly = true;
+            this.DC02Textbox.Size = new System.Drawing.Size(201, 20);
+            this.DC02Textbox.TabIndex = 6;
+            // 
+            // DC02Button
+            // 
+            this.DC02Button.Location = new System.Drawing.Point(145, 110);
+            this.DC02Button.Name = "DC02Button";
+            this.DC02Button.Size = new System.Drawing.Size(100, 23);
+            this.DC02Button.TabIndex = 9;
+            this.DC02Button.Text = "Sinh DC02";
+            this.DC02Button.UseVisualStyleBackColor = true;
+            this.DC02Button.Click += new System.EventHandler(this.DC02Button_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(428, 295);
-            this.Controls.Add(this.addressTextbox);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.loadButton);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.folderButton);
             this.Controls.Add(this.fileButton);
-            this.Controls.Add(this.folderLabel);
-            this.Controls.Add(this.folderTextbox);
             this.Controls.Add(this.fileLabel);
             this.Controls.Add(this.fileTextBox);
             this.Name = "Form1";
             this.Text = "896 Tool";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -154,6 +287,17 @@
         private System.Windows.Forms.Button folderButton;
         private System.Windows.Forms.Button loadButton;
         private System.Windows.Forms.TextBox addressTextbox;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Label checkLabel;
+        private System.Windows.Forms.TextBox checkTextbox;
+        private System.Windows.Forms.Button DC02FolderButton;
+        private System.Windows.Forms.Label DC02Label;
+        private System.Windows.Forms.TextBox DC02Textbox;
+        private System.Windows.Forms.Button DC02Button;
     }
 }
 
--- 896 Tool/Form1.cs
+++ 896 Tool/Form1.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 
@@ -36,14 +32,15 @@ namespace _896_Tool
         {
             if (folderTextbox.Text == null || folderTextbox.Text == "")
             {
-                MessageBox.Show("Chon thu muc");
+                MessageBox.Show("Chọn thư mục");
             }
             else
             {
                 string folderPath = folderTextbox.Text;
-                Excel.Application excelApplication;
+                Excel.Application excelApplication = new Excel.Application(); ;
                 Excel.Workbook workbook;
                 Excel.Worksheet worksheet;
+
                 foreach (KeyValuePair<int, Household> household in village)
                 {
                     string id = household.Key < 10 ? "0" + household.Key : household.Key.ToString();
@@ -53,7 +50,6 @@ namespace _896_Tool
                     string templateFile = @"\phuctra" + pageNum + ".xlsx";
 
                     File.Copy(Directory.GetCurrentDirectory() + templateFile, fileName, true);
-                    excelApplication = new Excel.Application();
                     workbook = excelApplication.Workbooks.Open(fileName);
                     worksheet = workbook.Worksheets[1];
                     int[] rows = { 2, 45 };
@@ -104,17 +100,44 @@ namespace _896_Tool
                         worksheet.get_Range(rName).Value = members[i].Name;
                     }
 
-                    worksheet.get_Range("D8:F11").Value = "D";
+                    string[,] dMarks = { { "D7:E16", "D18:E19", "D21:E22", "D24:E25", "D27:E28", "D30:E32", "D34:E35" },
+                                           { "I7:J16", "I18:J19", "I21:J22", "I24:J25", "I27:J28", "I30:J32", "I34:J35"},
+                                            { "N7:O16", "N18:O19", "N21:O22", "N24:O25", "N27:O28", "N30:O32", "N34:O35"},
+                                            { "S7:T16", "S18:T19", "S21:T22", "S24:T25", "S27:T28", "S30:T32", "S34:T35"},
+                                            { "X7:Y16", "X18:Y19", "X21:Y22", "X24:Y25", "X27:Y28", "X30:Y32", "X34:Y35"},
+                                            { "D50:E59", "D61:E62", "D64:E65", "D67:E68", "D70:E71", "D73:E75", "D77:E78" },
+                                           { "I50:J59", "I61:J62", "I64:J65", "I67:J68", "I70:J71", "I73:J75", "I77:J78"},
+                                            { "N50:O59", "N61:O62", "N64:J65", "N67:O68", "N70:O71", "N73:O75", "N77:O78"},
+                                            { "S50:T59", "S61:T62", "S64:T65", "S67:T68", "S70:T71", "S73:T75", "S77:T78"},
+                                            { "X50:Y59", "X61:Y62", "X64:Y65", "X67:Y68", "X70:Y71", "X73:Y75", "X77:Y78"}};
+
+                    for (int i = 0; i < members.Count; i++)
+                    {
+                        if (members[i].IsRegistered)
+                        {
+                            for (int j = 0; j < dMarks.GetLength(1); j++)
+                            {
+                                worksheet.get_Range(dMarks[i, j]).Value = "Đ";
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    
 
                     workbook.Save();
-                    GC.Collect();
-                    GC.WaitForPendingFinalizers();
-                    Marshal.ReleaseComObject(worksheet);
                     workbook.Close();
+                    Marshal.ReleaseComObject(worksheet);
                     Marshal.ReleaseComObject(workbook);
-                    excelApplication.Quit();
-                    Marshal.ReleaseComObject(excelApplication);
+                    
                 }
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                excelApplication.Quit();
+                Marshal.ReleaseComObject(excelApplication);
             }
         }
 
@@ -144,12 +167,14 @@ namespace _896_Tool
 
                 object[,] data = (object[,])range.Value2;
 
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
+                workbook.Close();
+
                 Marshal.ReleaseComObject(range);
                 Marshal.ReleaseComObject(worksheet);
-                workbook.Close();
                 Marshal.ReleaseComObject(workbook);
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
                 excelApplication.Quit();
                 Marshal.ReleaseComObject(excelApplication);
 
@@ -202,6 +227,114 @@ namespace _896_Tool
                     household.NumMembers = numMembers;
                     household.NumRegistered = numRegistered;
                 }
+            }
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            DialogResult result = fileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                checkTextbox.Text = fileDialog.FileName;
+            }
+        }
+
+        private void DC02FolderButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                DC02Textbox.Text = folderDialog.SelectedPath;
+            }
+        }
+
+        private void DC02Button_Click(object sender, EventArgs e)
+        {
+            string[,] checks = { { "C7:E19", "C21:E35"}, { "H7:J19", "H21:J35"}, { "M7:O19", "M21:O35" }, { "R7:T19", "R21:T35" }, { "W7:Y19", "W21:Y35" },
+                                    { "C50:E62", "C64:E78"}, { "H50:J62", "H64:J78"}, { "M50:O62", "M64:O78" }, { "R50:T62", "R64:T78" }, { "W50:Y62", "W64:Y78" }};
+            string[] nameLocations = { "C5", "H5", "M5", "R5", "W5", "C48", "H48", "M48", "R48", "W48" };
+            if (checkTextbox.Text == null || checkTextbox.Text == "")
+            {
+                MessageBox.Show("Chọn file");
+                return;
+            }
+            if (DC02Textbox.Text == null || DC02Textbox.Text == "")
+            {
+                MessageBox.Show("Chọn thư mục");
+                return;
+            }
+            string checkFileName = checkTextbox.Text;
+            string dc02FolderName = DC02Textbox.Text;
+
+            string fileName = Path.GetFileName(checkFileName);
+            string[] arr = fileName.Split(' ');
+            int householdId = int.Parse(arr[0]);
+
+            Household household = village[householdId];
+
+            Excel.Application excelApplication = new Excel.Application(); ;
+            Excel.Workbook workbook = excelApplication.Workbooks.Open(checkFileName);
+            Excel.Worksheet worksheet = worksheet = workbook.Worksheets[1];
+
+            List<string> corrections = new List<string>();
+
+            Excel.Range titles1 = worksheet.get_Range("B7:B19");
+            Excel.Range titles2 = worksheet.get_Range("B21:B35");
+
+            for (int i = 0; i < household.NumRegistered; i++)
+            {
+                Excel.Range range1 = worksheet.get_Range(checks[i, 0]);
+                Excel.Range range2 = worksheet.get_Range(checks[i, 1]);
+                object[,] data1 = (object[,]) range1.Value2;
+                object[,] data2 = (object[,]) range2.Value2;
+
+                for (int j = 1; j <= data1.GetLength(0); j++)
+                {
+                    for (int k = 1; k <= data1.GetLength(1); k++)
+                    {
+                        string d1 = data1[j, k] != null ? data1[j, k].ToString() : "x";
+                        if (d1.ToUpper() == "T" || d1.ToUpper() == "S")
+                        {
+                            string action = d1.ToUpper() == "T" ? "Thêm" : "Sửa";
+                            string newValue = range1.Cells[j, k].Comment != null ? range1.Cells[j, k].Comment.Text() : "";
+                            string correction = string.Format("{0} {1} thành: {2}", action, titles1.Cells[j, 1].Value.ToString(), newValue);
+                            corrections.Add(correction);
+                            break;
+                        }
+                    }
+                }
+
+                for (int j = 1; j <= data2.GetLength(0); j++)
+                {
+                    for (int k = 1; k <= data2.GetLength(1); k++)
+                    {
+                        string d2 = data2[j, k] != null ? data2[j, k].ToString() : "x";
+                        if (d2.ToUpper() == "T" || d2.ToUpper() == "S")
+                        {
+                            string action = d2.ToUpper() == "T" ? "Thêm" : "Sửa";
+                            string newValue = range2.Cells[j, k].Comment != null ? range2.Cells[j, k].Comment.Text() : "";
+                            string correction = string.Format("{0} {1} thành: {2}", action, titles2.Cells[j, 1].Value.ToString(), newValue);
+                            corrections.Add(correction);
+                            break;
+                        }
+                    }
+                }
+
+                
+            }
+
+            workbook.Close();
+            Marshal.ReleaseComObject(worksheet);
+            Marshal.ReleaseComObject(workbook);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            excelApplication.Quit();
+            Marshal.ReleaseComObject(excelApplication);
+
+            foreach (String s in corrections)
+            {
+                MessageBox.Show(s);
             }
         }
     }  
