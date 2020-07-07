10747c6c0e69f661675ea529bd614d36d14e3cab
admin <admin@ADMIN-PC>
2020-07-07 16:41:33 +0700

    1

--- DC02 tool/Form1.Designer.cs
+++ DC02 tool/Form1.Designer.cs
@@ -40,16 +40,20 @@
             this.addressTextbox = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.updateRichTextbox = new System.Windows.Forms.RichTextBox();
-            this.attachment = new System.Windows.Forms.RichTextBox();
+            this.attachmentRichTextbox = new System.Windows.Forms.RichTextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.householdIdTextbox = new System.Windows.Forms.TextBox();
+            this.toDocButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.fileTextbox = new System.Windows.Forms.TextBox();
+            this.browseFileButton = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 42);
+            this.label1.Location = new System.Drawing.Point(18, 60);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(26, 13);
             this.label1.TabIndex = 0;
@@ -57,7 +61,7 @@
             // 
             // nameTextbox
             // 
-            this.nameTextbox.Location = new System.Drawing.Point(79, 39);
+            this.nameTextbox.Location = new System.Drawing.Point(82, 57);
             this.nameTextbox.Name = "nameTextbox";
             this.nameTextbox.Size = new System.Drawing.Size(220, 20);
             this.nameTextbox.TabIndex = 1;
@@ -65,7 +69,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 82);
+            this.label2.Location = new System.Drawing.Point(18, 100);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 13);
             this.label2.TabIndex = 2;
@@ -75,7 +79,7 @@
             // 
             this.dobDatetimePicker.CustomFormat = "dd/MM/yyyy";
             this.dobDatetimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dobDatetimePicker.Location = new System.Drawing.Point(79, 82);
+            this.dobDatetimePicker.Location = new System.Drawing.Point(82, 100);
             this.dobDatetimePicker.Name = "dobDatetimePicker";
             this.dobDatetimePicker.Size = new System.Drawing.Size(220, 20);
             this.dobDatetimePicker.TabIndex = 3;
@@ -84,7 +88,7 @@
             // 
             this.groupBox1.Controls.Add(this.femaleRadioButton);
             this.groupBox1.Controls.Add(this.maleCheckbox);
-            this.groupBox1.Location = new System.Drawing.Point(18, 117);
+            this.groupBox1.Location = new System.Drawing.Point(21, 135);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(381, 74);
             this.groupBox1.TabIndex = 5;
@@ -115,15 +119,17 @@
             // 
             // idTextbox
             // 
-            this.idTextbox.Location = new System.Drawing.Point(79, 211);
+            this.idTextbox.Location = new System.Drawing.Point(82, 229);
+            this.idTextbox.MaxLength = 12;
             this.idTextbox.Name = "idTextbox";
             this.idTextbox.Size = new System.Drawing.Size(320, 20);
             this.idTextbox.TabIndex = 7;
+            this.idTextbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.idTextbox_KeyPress);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 214);
+            this.label3.Location = new System.Drawing.Point(18, 232);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 6;
@@ -131,7 +137,7 @@
             // 
             // addressTextbox
             // 
-            this.addressTextbox.Location = new System.Drawing.Point(79, 244);
+            this.addressTextbox.Location = new System.Drawing.Point(82, 262);
             this.addressTextbox.Name = "addressTextbox";
             this.addressTextbox.Size = new System.Drawing.Size(320, 20);
             this.addressTextbox.TabIndex = 9;
@@ -139,7 +145,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(15, 247);
+            this.label4.Location = new System.Drawing.Point(18, 265);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(40, 13);
             this.label4.TabIndex = 8;
@@ -147,24 +153,24 @@
             // 
             // updateRichTextbox
             // 
-            this.updateRichTextbox.Location = new System.Drawing.Point(18, 308);
+            this.updateRichTextbox.Location = new System.Drawing.Point(21, 326);
             this.updateRichTextbox.Name = "updateRichTextbox";
             this.updateRichTextbox.Size = new System.Drawing.Size(381, 73);
             this.updateRichTextbox.TabIndex = 10;
             this.updateRichTextbox.Text = "";
             // 
-            // attachment
+            // attachmentRichTextbox
             // 
-            this.attachment.Location = new System.Drawing.Point(18, 399);
-            this.attachment.Name = "attachment";
-            this.attachment.Size = new System.Drawing.Size(381, 72);
-            this.attachment.TabIndex = 11;
-            this.attachment.Text = "";
+            this.attachmentRichTextbox.Location = new System.Drawing.Point(21, 417);
+            this.attachmentRichTextbox.Name = "attachmentRichTextbox";
+            this.attachmentRichTextbox.Size = new System.Drawing.Size(381, 72);
+            this.attachmentRichTextbox.TabIndex = 11;
+            this.attachmentRichTextbox.Text = "";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(305, 42);
+            this.label5.Location = new System.Drawing.Point(308, 60);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(53, 13);
             this.label5.TabIndex = 12;
@@ -172,19 +178,62 @@
             // 
             // householdIdTextbox
             // 
-            this.householdIdTextbox.Location = new System.Drawing.Point(361, 39);
+            this.householdIdTextbox.Location = new System.Drawing.Point(364, 57);
             this.householdIdTextbox.Name = "householdIdTextbox";
             this.householdIdTextbox.Size = new System.Drawing.Size(60, 20);
             this.householdIdTextbox.TabIndex = 13;
+            this.householdIdTextbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.householdIdTextbox_KeyPress);
+            // 
+            // toDocButton
+            // 
+            this.toDocButton.Location = new System.Drawing.Point(176, 498);
+            this.toDocButton.Name = "toDocButton";
+            this.toDocButton.Size = new System.Drawing.Size(75, 23);
+            this.toDocButton.TabIndex = 14;
+            this.toDocButton.Text = "Lưu ra file";
+            this.toDocButton.UseVisualStyleBackColor = true;
+            this.toDocButton.Click += new System.EventHandler(this.toDocButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Chọn file";
+            // 
+            // fileTextbox
+            // 
+            this.fileTextbox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.fileTextbox.Location = new System.Drawing.Point(82, 24);
+            this.fileTextbox.Name = "fileTextbox";
+            this.fileTextbox.ReadOnly = true;
+            this.fileTextbox.Size = new System.Drawing.Size(220, 20);
+            this.fileTextbox.TabIndex = 16;
+            // 
+            // browseFileButton
+            // 
+            this.browseFileButton.Location = new System.Drawing.Point(311, 22);
+            this.browseFileButton.Name = "browseFileButton";
+            this.browseFileButton.Size = new System.Drawing.Size(75, 23);
+            this.browseFileButton.TabIndex = 17;
+            this.browseFileButton.Text = "Chọn file";
+            this.browseFileButton.UseVisualStyleBackColor = true;
+            this.browseFileButton.Click += new System.EventHandler(this.browseFileButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(433, 542);
+            this.Controls.Add(this.browseFileButton);
+            this.Controls.Add(this.fileTextbox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.toDocButton);
             this.Controls.Add(this.householdIdTextbox);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.attachment);
+            this.Controls.Add(this.attachmentRichTextbox);
             this.Controls.Add(this.updateRichTextbox);
             this.Controls.Add(this.addressTextbox);
             this.Controls.Add(this.label4);
@@ -218,9 +267,13 @@
         private System.Windows.Forms.TextBox addressTextbox;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.RichTextBox updateRichTextbox;
-        private System.Windows.Forms.RichTextBox attachment;
+        private System.Windows.Forms.RichTextBox attachmentRichTextbox;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox householdIdTextbox;
+        private System.Windows.Forms.Button toDocButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox fileTextbox;
+        private System.Windows.Forms.Button browseFileButton;
     }
 }
 
--- DC02 tool/Form1.cs
+++ DC02 tool/Form1.cs
@@ -3,18 +3,148 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace DC02_tool
 {
     public partial class Form1 : Form
     {
+        private string templateFile = Directory.GetCurrentDirectory() + @"\dc02.docx";
+        private string filePath;
+        //private Word.Application wordApplication;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void householdIdTextbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void idTextbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void toDocButton_Click(object sender, EventArgs e)
+        {
+            string name = nameTextbox.Text;
+            string householdId = householdIdTextbox.Text;
+            string dob = dobDatetimePicker.Value.ToString("ddMMyyyy");
+            bool gender = maleCheckbox.Checked ? true : false;
+            string id = idTextbox.Text;
+            string address = addressTextbox.Text;
+            string update = updateRichTextbox.Text;
+            string attachment = attachmentRichTextbox.Text;
+
+            if (filePath == "")
+            {
+                MessageBox.Show("Chọn file");
+                return;
+            }
+
+            if (name == "")
+            {
+                MessageBox.Show("Điền tên");
+                return;
+            }
+
+            string tempFile = Directory.GetCurrentDirectory() + @"\temp.docx";
+            File.Copy(templateFile, tempFile);
+            Word.Application wordApplication = new Word.Application();
+            Word.Document tempDoc = wordApplication.Documents.Open(tempFile);
+
+            FindAndReplace(tempDoc, "{name}", name);
+
+            char[] dobArr = dob.ToCharArray();
+            for (int i = 0; i < dobArr.Length; i++)
+            {
+                FindAndReplace(tempDoc, "{d" + i + "}", dobArr[i].ToString());
+            }
+
+            string checkLoc = gender ? "0" : "1";
+            string uncheckLoc = !gender ? "0" : "1";
+            FindAndReplace(tempDoc, "{s" + checkLoc + "}", "X");
+            FindAndReplace(tempDoc, "{s" + uncheckLoc + "}", "");
+
+            char[] idArr = id.ToCharArray();
+            for (int i = 0; i < idArr.Length; i++)
+            {
+                FindAndReplace(tempDoc, "{i" + i + "}", idArr[i].ToString());
+            }
+
+            FindAndReplace(tempDoc, "{address}", address);
+            FindAndReplace(tempDoc, "{update}", update);
+            FindAndReplace(tempDoc, "{attachment}", attachment);
+
+            tempDoc.Save();
+            Marshal.ReleaseComObject(tempDoc);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            wordApplication.Quit();
+            Marshal.ReleaseComObject(wordApplication);
+        }
+
+        private void browseFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "doc file (*.doc)|*.doc|docx file (*.docx)|*.docx";
+            DialogResult result = fileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                fileTextbox.Text = fileDialog.FileName;
+                filePath = fileTextbox.Text;
+
+                //wordApplication = new Word.Application();
+                //Word.Document generatedDocument = wordApplication.Documents.Open(filePath);
+                //Word.Document templateDocument = wordApplication.Documents.Open(templateFile);
+            }
+        }
+
+        private void FindAndReplace(Word.Document doc, object findText, object replaceWithText)
+        {
+            //options
+            object matchCase = false;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundsLike = false;
+            object matchAllWordForms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiacritics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+            //execute find and replace
+            
+
+            var range = doc.Range();
+
+            
+            range.Find.Execute(FindText: findText, ReplaceWith: replaceWithText, Replace: Word.WdReplace.wdReplaceAll);
+
+            var shapes = doc.Shapes;
+            
+
+            foreach (Word.Shape shape in shapes)
+            {
+                var initialText = shape.TextFrame.TextRange.Text;
+                var resultingText = initialText.Replace(findText.ToString(), replaceWithText.ToString());
+                shape.TextFrame.TextRange.Text = resultingText;
+
+            }
         }
     }
 }
